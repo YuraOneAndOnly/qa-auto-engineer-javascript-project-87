4cbb85d127f8bb4b67da64bd9518309158f55fe6
Yura <yury.kirytkin@yandex.ru>
2024-08-26 21:18:13 +0300

    Added arguments to CLI help

--- gendiff.js
+++ gendiff.js
@@ -2,6 +2,9 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format');
 program.parse();
