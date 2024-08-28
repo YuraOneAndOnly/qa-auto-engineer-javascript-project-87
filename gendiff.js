b4ebd4b422523c7592718aef8581c5cabbfd19cc
Yura <yury.kirytkin@yandex.ru>
2024-08-29 00:38:47 +0300

    Linting

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import compareJSONFiles from '../src/compareJSONFiles.js';
+import compareJSONFiles from './src/compareJSONFiles.js';
 
 const program = new Command();
 program
