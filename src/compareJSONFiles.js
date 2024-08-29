c8a8d73b4c1ddbdaa27c28d1b261018365c73cdc
Yura <yury.kirytkin@yandex.ru>
2024-08-29 22:21:05 +0300

    Moved path.resolve to importFile

--- src/compareJSONFiles.js
+++ src/compareJSONFiles.js
@@ -1,10 +1,9 @@
-import path from 'node:path';
 import importFile from './importFile.js';
 import compareTwoFlatJSON from './compareTwoFlatJSON.js';
 
 export default function compareJSONFiles(rawPathToFile1, rawPathToFile2) {
-  const fileContent1 = importFile(path.resolve(rawPathToFile1));
-  const fileContent2 = importFile(path.resolve(rawPathToFile2));
+  const fileContent1 = importFile(rawPathToFile1);
+  const fileContent2 = importFile(rawPathToFile2);
   const json1 = JSON.parse(fileContent1);
   const json2 = JSON.parse(fileContent2);
   const result = compareTwoFlatJSON(json1, json2);
--- src/importFile.js
+++ src/importFile.js
@@ -1,8 +1,10 @@
 import { readFileSync } from 'node:fs';
 import jschardet from 'jschardet';
+import path from 'node:path';
 
-export default function importFile(pathToFile) {
+export default function importFile(rawPathToFile) {
   try {
+    const pathToFile = path.resolve(rawPathToFile);
     const fileHexContent = readFileSync(pathToFile);
     const detectedEncoding = jschardet.detect(fileHexContent);
     const fileContent = readFileSync(pathToFile, detectedEncoding.encoding);
