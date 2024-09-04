6d58a9a1480c87f972c11a8819251cc7a734f624
Yura <yury.kirytkin@yandex.ru>
2024-09-04 23:18:53 +0300

    Test

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 // import { Command } from 'commander';
-import genDiff from '../src/genDiff.js';
+import gendiff from '../src/genDiff.js';
 
-export default (rawPathToFile1, rawPathToFile2, formatRawName = 'stylish') => genDiff(rawPathToFile1, rawPathToFile2, formatRawName);
+export default (rawPathToFile1, rawPathToFile2, formatRawName = 'stylish') => gendiff(rawPathToFile1, rawPathToFile2, formatRawName);
 
 /*
 const program = new Command();
