af7991e87f46aa095947e5cf5bb91dddbc0aad5c
Yura <yury.kirytkin@yandex.ru>
2024-09-04 22:55:03 +0300

    Changed CRLF to LF in /bin/index.js

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+// #!/usr/bin/env node
+
+// import { Command } from 'commander';
+import genDiff from '../src/genDiff.js';
+
+export default (rawPathToFile1, rawPathToFile2, formatRawName = 'stylish') => genDiff(rawPathToFile1, rawPathToFile2, formatRawName);
+
+/*
+const program = new Command();
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish');
+program.action(gendiff);
+program.parse();
+*/
